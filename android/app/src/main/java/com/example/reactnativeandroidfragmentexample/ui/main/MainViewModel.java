83c6f5665f5a4c1c875ec7b973e7d4da7d77ffd5
svnm <stevenisekimartin@gmail.com>
2020-09-02 15:13:31 +1000

    Initial react-native android setup

---
+++ android/app/src/main/java/com/example/reactnativeandroidfragmentexample/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.reactnativeandroidfragmentexample.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
