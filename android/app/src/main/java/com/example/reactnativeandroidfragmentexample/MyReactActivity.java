d1d316624968aae29f2d177b9671ad70036ec5b5
Sebastien Bailouni <sbailouni@gmail.com>
2020-09-03 11:04:34 +1000

    Remove com.facebook.BuildConfig import

--- android/app/src/main/java/com/example/reactnativeandroidfragmentexample/MyReactActivity.java
+++ android/app/src/main/java/com/example/reactnativeandroidfragmentexample/MyReactActivity.java
@@ -1,10 +1,12 @@
 package com.example.reactnativeandroidfragmentexample;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.KeyEvent;
 
-import com.facebook.react.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactPackage;
